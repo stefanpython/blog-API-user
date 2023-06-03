33ab7cb25e0b5683f53f42ac7af7fce268f3559b
stefanpython <stefanhtml@yahoo.com>
2023-06-03 12:04:56 +0300

    Updates

---
+++ precache-manifest.28b06f205472d380855aac370ae269d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7da99e55c51af1a2d56a",
+    "url": "/blog-user-api/static/css/main.40a15c71.chunk.css"
+  },
+  {
+    "revision": "7da99e55c51af1a2d56a",
+    "url": "/blog-user-api/static/js/main.7da99e55.chunk.js"
+  },
+  {
+    "revision": "66497827e121027b9280",
+    "url": "/blog-user-api/static/js/1.66497827.chunk.js"
+  },
+  {
+    "revision": "883504f5f5ac314a58b0",
+    "url": "/blog-user-api/static/js/runtime~main.883504f5.js"
+  },
+  {
+    "revision": "396748fbc3051cec39fa9e1a05455986",
+    "url": "/blog-user-api/index.html"
+  }
+];
